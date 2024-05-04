762eab43474c4ccd2c9066499d3a9afee3374686
MohammadSameh <167872264+MohammadSameh@users.noreply.github.com>
2024-05-04 16:25:05 +0300

    Update Student.java

--- src/main.java.com.byteWise/users/Student.java
+++ src/main.java.com.byteWise/users/Student.java
@@ -11,7 +11,7 @@ package src.main.java.com.byteWise.users;
 import java.util.List;
 
 import src.main.java.com.byteWise.courses.Course;
-import src.main.java.com.byteWise.filesystem.Read_Write;
+
 
 import java.util.ArrayList;
 public class Student extends User {
@@ -28,15 +28,14 @@ public class Student extends User {
         System.out.println("Name: " + getName());
         System.out.println("Enrolled Courses:");
         for (Course course : courses) {
-            course.getCourseTitle();
+            course.getTitle();
         }
     }
 
     public void enrollInCourse(Course course) throws CourseNotFoundException {
         if (!courses.contains(course)) {
             courses.add(course);
-            Read_Write.writeToJson(this);
-            System.out.println("Enrolled in course: " + course.getCourseTitle());
+            System.out.println("Enrolled in course: " + course.getTitle());
         } else if (courses.contains(course)){
             throw new CourseNotFoundException("Can't enroll in course. Course already enrolled.");
         } else {
@@ -47,8 +46,7 @@ public class Student extends User {
     public void dropCourse(Course course) throws CourseNotFoundException{
         if (courses.contains(course)) {
             courses.remove(course);
-            Read_Write.writeToJson(this);
-            System.out.println("Dropped course: " + course.getCourseTitle());
+            System.out.println("Dropped course: " + course.getTitle());
         } else {
             throw new CourseNotFoundException("Can't drop course. Course not found.");
         }
